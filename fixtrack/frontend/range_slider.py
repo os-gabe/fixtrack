8aa0f7bc76906cee4c12e1c714ee3b0978c96621
Gabriel Hein <gabe@openspace.ai>
2020-10-25 14:04:48 -0700

    Fixes #31 'Crash while using crop range slider'

--- fixtrack/frontend/range_slider.py
+++ fixtrack/frontend/range_slider.py
@@ -132,7 +132,7 @@ class RangeSlider(QtWidgets.QWidget):
         )
 
         if (self._first_sc == self.range_slider_handle):
-            if (pos <= self.second_position) or (self.last_clicked == 0):
+            if pos <= self.second_position:
                 self.first_position = pos
                 self.update()
                 self.sliderMoved.emit(self.first_position, self.second_position, 0)
